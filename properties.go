071987569b3d91c286dc63a8f5f46d11434d153c
Sam Rajan <samjerusalem@gmail.com>
2022-08-08 20:57:16 +0100

    Add the sample kafka producer

--- properties.go
+++ properties.go
@@ -5,22 +5,16 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type KafkaProperties struct {
-	bootstrapServer  string
-	securityProtocol string
-	sasl             SecurityLayer
+	value kafka.ConfigMap
 }
 
-type SecurityLayer struct {
-	mechanism string
-	userName  string
-	password  string
-}
-
-func (self *KafkaProperties) LoadProperties() {
-	configFile, err := os.Open("go-kafka-messaging.properties")
+func (self *KafkaProperties) LoadProperties(configFileName string) {
+	configFile, err := os.Open(configFileName)
 
 	if err != nil {
 		fmt.Printf("Failed to read config file")
@@ -28,25 +22,17 @@ func (self *KafkaProperties) LoadProperties() {
 	}
 
 	scanner := bufio.NewScanner(configFile)
+	self.value = make(map[string]kafka.ConfigValue)
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
 		if !strings.HasPrefix(line, "#") && len(line) != 0 {
 			keyval := strings.Split(line, "=")
-			self.sasl = SecurityLayer{}
 
-			switch keyval[0] {
-			case "bootstrap.servers":
-				self.bootstrapServer = keyval[1]
-			case "security.protocol":
-				self.securityProtocol = keyval[1]
-			case "sasl.mechanisms":
-				self.sasl.mechanism = keyval[1]
-			case "sasl.password":
-				self.sasl.userName = keyval[1]
-			case "sasl.username":
-				self.sasl.userName = keyval[1]
-			}
+			parameter := strings.TrimSpace(keyval[0])
+			value := strings.TrimSpace(keyval[1])
+
+			self.value[parameter] = value
 		}
 
 		if err := scanner.Err(); err != nil {
