24e8b2eef91147d84886d008760260c4ddffadb7
Sam Rajan <samjerusalem@gmail.com>
2022-09-01 23:37:07 +0100

    refactor input parser

--- internal/app/sender/input-parser/parser.go
+++ internal/app/sender/input-parser/parser.go
@@ -1,73 +1,16 @@
 package inputparser
 
-import (
-	"errors"
-	"strings"
+type Parser interface {
+	Parse(text string)
+}
+
+const (
+	ACTION_COMMAND = "COMMAND"
+	ACTION_MESSAGE = "MESSAGE"
 )
 
-type Action string
-
 type Message struct {
-	topic   string
-	payload *Payload
-}
-
-type Payload struct {
-	Action  Action
-	Message string
-}
-
-func (self *Message) GetTopic() string {
-	return self.topic
-}
-
-func (self *Message) GetPayload() *Payload {
-	return self.payload
-}
-
-func ParseMessage(input string) (*Message, error) {
-
-	receiver, err := "", error(nil)
-	if string(input[0]) == "@" {
-		receiver, input, err = parseReceiver(input)
-	}
-
-	if err != nil {
-		return new(Message), err
-	}
-
-	data := &Payload{}
-	if isAction(input) {
-		data.Action = Action(input)
-	} else {
-		data.Message = input
-	}
-
-	if receiver == "" && data.Message != "" {
-		return new(Message), errors.New("Message without recepient is not possible")
-	}
-
-	return &Message{topic: receiver, payload: data}, nil
-}
-
-func parseReceiver(input string) (string, string, error) {
-	receiverEnd := strings.Index(input, " ")
-
-	if receiverEnd == -1 {
-		return "", input, errors.New("Expecting a recepient reference")
-	}
-
-	return input[1:receiverEnd], input[receiverEnd+1:], nil
-}
-
-func isAction(input string) bool {
-
-	actionList := []string{"EXIT", "HISTORY"}
-	for _, v := range actionList {
-		if v == strings.ToUpper(input) {
-			return true
-		}
-	}
-
-	return false
+	Type     string
+	Receiver string
+	Value    string
 }
